5a2927758eebf80bca56c2bb7b70879fa1372290
Zaliappara <124375115+VladislavVorobev@users.noreply.github.com>
2023-12-03 17:08:50 +0200

    Mission Complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat2);
+
     }
 }
